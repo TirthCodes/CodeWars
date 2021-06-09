56b030ed595f6e7fa1b06eb36eb68407eea61c93
Tirth Patel <tirthpatelcse@gmail.com>
2021-06-09 17:15:47 +0530

    Update vowelCount.js
    Added Case-2 to count Vowels in a string using Regex.

--- vowelCount.js
+++ vowelCount.js
@@ -2,11 +2,11 @@
 We will consider a, e, i, o, u as vowels for this Kata (but not y).
 The input string will only consist of lower case letters and/or spaces.*/
 
+//Case-1 Using for loop
 const vowels = ["a", "e", "i", "o", "u"]
 function getCount(str) {
   let vowelsCount = 0;
   
-  // enter your majic here
   for (let letter of str.toLowerCase()) {
         if (vowels.includes(letter)) {
             vowelsCount++;
@@ -17,4 +17,14 @@ function getCount(str) {
 
 const string = prompt('Enter a string: ');
 const result = getCount(string);
+console.log(result);
+
+//Case-2 Using Regex
+function countVowel(str) { 
+    const count = str.match(/[aeiou]/gi).length;
+    return count;
+}
+
+const string = prompt('Enter a string: ');
+const result = countVowel(string);
 console.log(result);
